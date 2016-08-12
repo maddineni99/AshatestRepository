4362554a038ba59c0af32f868a07b16ed5303656
maddinen <maddinen@wharton.upenn.edu>
2016-08-12 14:31:22 -0400

    test

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello test");
+            Console.WriteLine("Hello asha");
         }
     }
 }
